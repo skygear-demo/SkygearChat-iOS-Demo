36612fcf96d6ecd3855430ba3efe9387faed7c51
Zachary <tszkanlo@gmail.com>
2017-09-25 17:16:32 +0800

    Initial Commit

---
+++ Swift Chat Demo 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Chat Demo 2
+//
+//  Created by Zachary on 25/9/2017.
+//  Copyright © 2017 USThing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
